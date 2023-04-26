1f4cdcf32fe737d2facb9500bc506ab80edf4559
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-04-26 23:36:55 +0300

    Created the Category model.

---
+++ NewsApiV2/NewsApiV2/Controllers/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NewsApiV2.Controllers.Models
+{
+	public class Category
+	{
+		public Guid Id { get; set; }
+		public string name { get; set; }
+		public Category()
+		{
+			
+		}
+	}
+}
+
