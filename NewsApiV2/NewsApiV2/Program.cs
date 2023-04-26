73f3d255d1b725b86ac9bfcbafc13f89533ef31f
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-04-26 19:38:15 +0300

    Added the comments path for the Swagger JSON and UI.

--- NewsApiV2/NewsApiV2/Program.cs
+++ NewsApiV2/NewsApiV2/Program.cs
@@ -1,11 +1,19 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( (c) =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+});
 
 var app = builder.Build();
 
