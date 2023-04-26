7efb08011a491849986d3b0293a822583451379c
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-04-26 23:50:31 +0300

    Added comments to the functions, hardcoded a few categories and made a few changees.

--- NewsApiV2/NewsApiV2/Controllers/CategoriesController.cs
+++ NewsApiV2/NewsApiV2/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NewsApiV2.Controllers.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,18 +13,38 @@ namespace NewsApiV2.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        static List<Category> categories = new List<Category> {
+            new Category { Id = Guid.NewGuid(), Name = "Lab"},
+            new Category { Id = Guid.NewGuid(), Name = "Course"},
+            new Category { Id = Guid.NewGuid(), Name = "Seminary"}
+        };
+
+        /// <summary>
+        /// This is an getAllCategories method.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Get()
         {
             return Ok("GetCategories method was called.");
         }
 
+        /// <summary>
+        /// This is an getCategoryById method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("getById/{id}")]
         public IActionResult GetById(Guid id)
         {
             return Ok("GetCategoryById method was called.");
         }
 
+        /// <summary>
+        /// This is an deleteCategoryById method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public IActionResult Delete([FromRoute] Guid id)
         {
--- NewsApiV2/NewsApiV2/Controllers/Models/Category.cs
+++ NewsApiV2/NewsApiV2/Controllers/Models/Category.cs
@@ -4,7 +4,7 @@ namespace NewsApiV2.Controllers.Models
 	public class Category
 	{
 		public Guid Id { get; set; }
-		public string name { get; set; }
+		public string Name { get; set; }
 		public Category()
 		{
 			
