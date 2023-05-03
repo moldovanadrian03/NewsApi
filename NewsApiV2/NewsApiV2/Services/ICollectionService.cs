0e47264bd91296ba12b45eb8f015edf48d8bdca8
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-03 17:11:18 +0300

    Created the services folder, IcollectionService interface and added some methods to it.

---
+++ NewsApiV2/NewsApiV2/Services/ICollectionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace NewsApiV2.Services
+{
+	public interface ICollectionService<T> where T: 
+	{
+        List<T> GetAll();
+
+        T Get(Guid id);
+
+        bool Create(T model);
+
+        bool Update(Guid id, T model);
+
+        bool Delete(Guid id);
+
+    }
+}
+
