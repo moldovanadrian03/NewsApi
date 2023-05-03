38f67d2ed2f39a4178a927a7749c111f710c1cc8
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-03 17:14:56 +0300

    Created AnnouncementCollection interface.

---
+++ NewsApiV2/NewsApiV2/Services/IAnnouncementCollectionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NewsApiV2.Controllers.Models;
+
+namespace NewsApiV2.Services
+{
+	public interface IAnnouncementCollectionService : ICollectionService<Announcement> 
+	{
+        List<Announcement> GetAnnouncementsByCategoryId(string categoryId);
+    }
+}
+
