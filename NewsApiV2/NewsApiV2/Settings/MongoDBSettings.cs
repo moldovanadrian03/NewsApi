b360b097af9561b8c71ea77adb25ecdeef49d4d3
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-10 19:09:54 +0300

    Created MongoDBSettings class wich implements IMongoDBSettings interface.

---
+++ NewsApiV2/NewsApiV2/Settings/MongoDBSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NewsApiV2.Settings
+{
+	public class MongoDBSettings : IMongoDBSettings
+	{
+        public string AnnouncementsCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
+
