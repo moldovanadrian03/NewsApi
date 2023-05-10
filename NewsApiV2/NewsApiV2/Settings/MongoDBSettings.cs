e74e5a94af9dfa740e1238e3e2c33e16ec194cca
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-10 20:12:52 +0300

    Removed the throws from the MongoDBSettings properties.

--- NewsApiV2/NewsApiV2/Settings/MongoDBSettings.cs
+++ NewsApiV2/NewsApiV2/Settings/MongoDBSettings.cs
@@ -3,9 +3,9 @@ namespace NewsApiV2.Settings
 {
 	public class MongoDBSettings : IMongoDBSettings
 	{
-        public string AnnouncementsCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string AnnouncementsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
     }
 }
 
